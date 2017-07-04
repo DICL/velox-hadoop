de3a027206c63470b5c928152d51904c38a2af52
deukyeon <deukyeon.hwang@gmail.com>
2017-07-04 14:50:42 +0900

    removed unrelevant files

--- resources/vagrant/ceph-repo-install.sh
+++
@@ -1,9 +0,0 @@
-cat >> /etc/yum.repos.d/ceph.repo << EOF
-[ceph-noarch]
-name=Ceph noarch packages
-baseurl=http://ceph.com/rpm-firefly/rhel6/noarch
-enabled=1
-gpgcheck=1
-type=rpm-md
-gpgkey=https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc
-EOF
